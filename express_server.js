f272f64933987d5da40939990f9864b458d30716
TJ <teejaynam@gmail.com>
2023-06-23 18:17:13 -0400

    update to add get for path /urls, to render urls_index

--- express_server.js
+++ express_server.js
@@ -7,10 +7,15 @@ app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}
+};
 
 app.get("/", (req,res) => {
   res.send("Hello!");
+});
+
+app.get("/urls", (req,res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars );
 });
 
 app.get("/urls.json", (req, res) => {
