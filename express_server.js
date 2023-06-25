c8cc784f95920cd91b78ba2886dce4d9fa575726
TJ <teejaynam@gmail.com>
2023-06-25 15:52:15 -0400

    update to add post request that stores long url and short url id key value pair to database, also add redirect to when accessing path /u/:id

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+//our alphanumeric random string generator for shortened urls 
 function generateRandomString() {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let randomString = '';
@@ -17,35 +18,52 @@ function generateRandomString() {
   return randomString;
 }
 
-
-const urlDatabase = {
+//the database for our urls
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const templateVars = { urls: urlDatabase };
+
+//root page 
 app.get("/", (req,res) => {
   res.send("Hello!");
 });
 
+//urls db page
 app.get("/urls", (req,res) => {
-  const templateVars = { urls: urlDatabase };
+  //const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars );
 });
 
+//post req to urls appends to our url db with a new shortened url
 app.post("/urls", (req, res) => {
-  let randomString = generateRandomString();
-  console.log(req.body, randomString); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let id = generateRandomString();
+  let postUrl = req.body.longURL;
+
+  //update database
+  urlDatabase[id] = postUrl;
+
+  //add redirect to use new id
+  res.redirect(`/urls/${id}`);
 });
 
+//page to do post request
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//page to show long url version of shortened url with path /urls/:id
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(`${longURL}`)
+})
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
