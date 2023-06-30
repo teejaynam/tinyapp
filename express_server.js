d3276b6a0f2a5ad78d02ea79016e671340025ad2
TJ <teejaynam@gmail.com>
2023-06-30 19:15:15 -0400

    add userId key to URL database to only show URLs created by respective users and update post requests for deleting and editing shortened URLs to check for if a user is logged in, authenticated or if the URL id exists

--- express_server.js
+++ express_server.js
@@ -7,33 +7,20 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }),cookieParser());
 
-//our alphanumeric random string generator for shortened urls
-function generateRandomString() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let randomString = '';
-  
-  for (let i = 0; i < 6; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    randomString += characters[randomIndex];
-  }
-  
-  return randomString;
-}
-
-//check email in user db
-function checkEmail(email) {
-  for (let userId in users) {
-    if (users[userId].email === email) {
-      return userId;
-    }
-  }
-  return false;
-}
-
 //the database for our urls
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2" : {
+    longURL: "http://www.lighthouselabs.ca",
+    userId: "user2RandomID",
+  },
+  "9sm5xK" : {
+    longURL: "http://www.google.com",
+    userId: "userRandomID",
+  },
+  "111111" : {
+    longURL: "http://www.youtube.com",
+    userId: "user2RandomID",
+  },
 };
 
 //the database for users
@@ -50,6 +37,51 @@ const users = {
   },
 };
 
+//6 character alphanumeric random string generator for shortened urls
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters[randomIndex];
+  }
+  
+  return randomString;
+}
+
+//function to check email in user db
+function checkEmail(email) {
+  for (let userId in users) {
+    if (users[userId].email === email) {
+      return userId;
+    }
+  }
+  return false;
+}
+
+//function that returns object of urls made by a specific user
+function urlsForUser(id) {
+  const matchingURLs = {};
+  for (let urlID in urlDatabase) {
+
+    const url = urlDatabase[urlID];
+
+    if (url.userId === id) {
+
+      const urlDB = {
+        [urlID] : {
+          longURL : url.longURL,
+          userId : url.userId
+        }
+      };
+
+      Object.assign(matchingURLs, urlDB);
+    }
+  }
+  return matchingURLs;
+}
+
 //root page, redirect to urls
 app.get("/", (req,res) => {
   if (users[req.cookies.user_id]) {
@@ -61,40 +93,77 @@ app.get("/", (req,res) => {
 
 //urls db page
 app.get("/urls", (req,res) => {
-  if (users[req.cookies.user_id]) {
-    const templateVars = { urls: urlDatabase, email : users[req.cookies.user_id].email };
+  const loggedOnUser = req.cookies.user_id;
+
+  if (users[loggedOnUser]) {
+    const urlsToShow = urlsForUser(loggedOnUser);
+    const templateVars = { urls: urlsToShow, email : users[req.cookies.user_id].email };
     res.render('urls_index', templateVars);
   } else {
-    res.redirect("/login");
+    res.status(401).send("You must login to view URLs -> /login");
   }
 });
 
 //post req to urls appends to our url db with a new shortened url
 app.post("/urls", (req, res) => {
-  if (users[req.cookies.user_id]) {
+  const loggedOnUser = req.cookies.user_id;
+
+  if (loggedOnUser) {
     let id = generateRandomString();
     let postUrl = req.body.longURL;
   
     //update database
-    urlDatabase[id] = postUrl;
+    const newURL = {
+      longURL : postUrl,
+      userId : loggedOnUser,
+    };
+
+    urlDatabase[id] = newURL;
   
     //add redirect to use new id
     res.redirect(`/urls/${id}`);
   } else {
-    res.send("You must login to shorten URLs");
+    res.status(401).send("You must login to shorten URLs");
   }
 });
 
 //post req to update urls but keep the id
 app.post("/urls/:id", (req,res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect("/urls");
+  const id = req.params.id;
+  const newURL = req.body.longURL;
+
+  if (!users[req.cookies.user_id]) {
+    res.status(401).send("401 You must log in to update URLs");
+  } else if (users[req.cookies.user_id].id !== urlDatabase[id].userId) {
+    res.status(403).send("403 Forbidden to UPDATE someone else's URL");
+  } else {
+    if (newURL) {
+      urlDatabase[req.params.id].longURL = req.body.longURL;
+      res.redirect("/urls");
+    } else {
+      res.status(400).send("bad request");
+    }
+  }
 });
 
 //post req to delete urls from db
 app.post("/urls/:id/delete", (req,res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect("/urls");
+  const id = req.params.id;
+  const deleteURL = urlDatabase[id] ? true : false;
+  
+  if (!users[req.cookies.user_id]) {
+    res.status(401).send("401 You must log in to delete");
+  } else if (users[req.cookies.user_id].id !== urlDatabase[id].userId) {
+    res.status(403).send("403 Forbidden to delete someone else's URL");
+  } else {
+    if (deleteURL) {
+      delete urlDatabase[req.params.id];
+      res.redirect("/urls");
+    } else {
+      res.status(404).send("website not found");
+    }
+  }
+
 });
 
 //page to do post request
@@ -110,27 +179,27 @@ app.get("/urls/new", (req, res) => {
 //page to show long url version of shortened url with path /urls/:id
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const url = urlDatabase[id];
+  const url = urlDatabase[id] ? urlDatabase[id].longURL : undefined;
 
-  if (url) {
-    if (users[req.cookies.user_id]) {
-      const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], email : users[req.cookies.user_id].email };
-      res.render("urls_show", templateVars);
-    } else {
-      res.redirect("/login")
-    }
+  if (!url) {
+    res.status(404).send("404 Not Found");
+  } else if (!users[req.cookies.user_id]) {
+    res.status(401).send("401 You must log in");
+  } else if (users[req.cookies.user_id].id !== urlDatabase[id].userId) {
+    res.status(403).send("403 Forbidden to edit someone else's URL");
   } else {
-    res.status(404).send("404 not found");
+    const templateVars = { id: req.params.id, longURL: url, email: users[req.cookies.user_id].email };
+    res.render("urls_show", templateVars);
   }
 });
+
 
 //redirect to the long url
 app.get("/u/:id", (req, res) => {
   const id = req.params.id;
-  const url = urlDatabase[id];
-  if (url){
-    const longURL = urlDatabase[req.params.id];
-    res.redirect(longURL);
+  const url = urlDatabase[id].longURL;
+  if (url) {
+    res.redirect(url);
   } else {
     res.status(404).send("404 - not a shortened url");
   }
@@ -190,22 +259,20 @@ app.post("/register", (req,res) => {
   if (email === "" || password === "") {
     res.status(400);
     res.send("Empty email or password");
-  }
-
-  //if checkEmail is truthy, then email exists, and we return error code
-  if (checkEmail(email)) {
+  } else if (checkEmail(email)) {   //if checkEmail is truthy, then email exists, and we return error code
     res.status(400);
     res.send("Email already exists");
+  } else {
+    const newUser = {
+      id : userId,
+      email : email,
+      password : password
+    };
+  
+    users[newUser.id] = newUser;
+  
+    res.redirect("/login");
   }
-
-  const newUser = {
-    id : userId,
-    email : email,
-    password : password
-  };
-
-  users[newUser.id] = newUser;
-  res.redirect("/login");
 });
 
 app.get("/urls.json", (req, res) => {
